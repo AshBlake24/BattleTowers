8035eff414573554a3c619885650df1227622da6
AshBlake24 <107277536+AshBlake24@users.noreply.github.com>
2022-09-27 18:17:51 +0300

    Rework test animation

--- Assets/Scripts/Enemy/State Machine/States/MoveState.cs
+++ Assets/Scripts/Enemy/State Machine/States/MoveState.cs
@@ -1,20 +1,34 @@
 using UnityEngine;
 
 [RequireComponent(typeof(WaypointFollower))]
+[RequireComponent(typeof(Animator))]
 public class MoveState : State
 {
+    private const string Moving = "Moving";
     private const float RotationTime = 0.1f;
 
     [SerializeField] private float _speed;
 
+    private Animator _animator;
     private WaypointFollower _follower;
     private Transform _target;
     private Vector3 _direction;
     private float _currentVelocity;
 
-    private void Start()
+    private void Awake()
     {
         _follower = GetComponent<WaypointFollower>();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _animator.SetBool(Moving, true);
+    }
+
+    private void OnDisable()
+    {
+        _animator.SetBool(Moving, false);
     }
 
     private void Update()
